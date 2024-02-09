680816c2de17b133a78f5f4326f24a573501ed38
mansha <nigwal.1998@gmail.com>
2024-02-10 02:33:44 +0530

    adding basic nextjs project and learning of router in nextjs

---
+++ basic-nextjs/src/app/product/[productId]/reviews/[reviewId]/page.js
@@ -0,0 +1,3 @@
+export default function ReviewDetails({params}){
+    return <h1 className="">Review {params.reviewId} of Product {params.productId}</h1>
+}
---
+++ basic-nextjs/src/app/product/page.js
@@ -0,0 +1,11 @@
+export default function Product(){
+    return (
+        <div className="">
+
+            <h1 className="">Products</h1>
+            <h3 className="">Product 1</h3>
+            <h3 className="">Product 2</h3>
+            <h3 className="">Product 3</h3>
+        </div>
+    )
+}
